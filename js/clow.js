a2bb3e3a72d2e981aba7f1067453fa87f0f94c2a
Choppychan <54774187+Choppychan@users.noreply.github.com>
2019-10-24 13:32:53 +0200

    Add files via upload

---
+++ js/clow.js
@@ -0,0 +1,56 @@
+function getWeather(){
+    var weatherResponse = $('.weatherResponse');
+    var weatherMain = $('.weatherMain');
+    var temp = $('.temp');
+    var country = $('.country');
+    var temp_max = $('.tempMax');
+    var temp_min = $('.tempMin');
+
+
+    weatherResponse.html('');
+    weatherMain.html('');
+    temp.html('');
+    temp_min.html('');
+    temp_max.html('');
+    country.html('');
+
+
+    var cityName = $('#cityName').val();
+    var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=20ce837a3f500c892b957257bbc630b8';
+
+    $.getJSON(apiCall, weatherCallback);
+
+    function weatherCallback(weatherData){
+        var cityName = weatherData.name;
+        var c = weatherData.sys.country;
+        var t = parseInt(weatherData.main.temp);
+        var t_min = parseInt(weatherData.main.temp_min);
+        var t_max = parseInt(weatherData.main.temp_max);
+        var weather_main = weatherData.weather[0].main;
+        if (weather_main.includes("Clouds")) {
+            weatherMain.addClass('fas fa-cloud');
+        } else if (weather_main.includes("Clear")) {
+            weatherMain.addClass('fas fa-sun');
+        } else if (weather_main.includes("Snow")) {
+          weatherMain.addClass('far fa-snowflake');
+        } else if (weather_main.includes("Rain")) {
+          weatherMain.addClass('fas fa-cloud-showers-heavy');
+        } else if (weather_main.includes("Drizzle")) {
+          weatherMain.addClass('fas fa-cloud-rain');
+        } else if (weather_main.includes("Thunderstorm")) {
+          weatherMain.addClass('fas fa-bolt');
+        } else if (weather_main.includes("Smoke") || weather_main.includes("Fog") || weather_main.includes("Mist") || weather_main.includes("Haze") || weather_main.includes("Dust") || weather_main.includes ("Sand") || weather_main.includes("Ash") || weather_main.includes("Squall") || weather_main.includes("Tornado")) {
+          weatherMain.addClass('fas fa-smog');
+        }
+
+        weatherResponse.append(cityName);
+        country.append(c);
+        temp.append(t);
+        temp_max.append(t_max);
+        temp_min.append(t_min);
+        weatherMain.append('');
+        $('#temp-table').removeClass('hide');
+    }
+
+
+}
